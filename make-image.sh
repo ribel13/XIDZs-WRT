5978ae9528baa29adf026e53cd05c94aea995fd7
Atho Belo <181945610+ribel13@users.noreply.github.com>
2026-01-13 16:21:52 +0900

    Update make-image.sh

--- make-image.sh
+++ make-image.sh
@@ -27,8 +27,8 @@ PACKAGES+=" kmod-usb-storage kmod-scsi-core dosfstools fdisk parted losetup resi
 PACKAGES+=" kmod-usb-uhci kmod-usb-ohci kmod-usb2 kmod-usb-ehci kmod-usb3 kmod-usb-xhci-hcd usbutils \
 kmod-macvlan kmod-mii kmod-usb-net kmod-usb-net-rtl8150 kmod-usb-net-rtl8152 kmod-usb-net-asix kmod-usb-net-asix-ax88179 \
 kmod-usb-serial kmod-usb-serial-option kmod-nls-utf8 kmod-usb-serial-wwan \
-kmod-usb-serial-qualcomm kmod-usb-serial-sierrawireless kmod-usb-acm kmod-usb-wdm \
-kmod-usb-net-rndis kmod-usb-net-cdc-ether kmod-usb-net-cdc-ncm kmod-usb-net-sierrawireless \
+kmod-usb-serial-qualcomm kmod-usb-acm kmod-usb-wdm \
+kmod-usb-net-rndis kmod-usb-net-cdc-ether kmod-usb-net-cdc-ncm \
 kmod-usb-net-qmi-wwan kmod-usb-net-huawei-cdc-ncm kmod-usb-net-cdc-mbim \
 uqmi libqmi qmi-utils umbim libmbim mbim-utils luci-proto-qmi luci-proto-ncm \
 modemmanager luci-proto-modemmanager usb-modeswitch xmm-modem luci-proto-xmm"
@@ -36,14 +36,15 @@ modemmanager luci-proto-modemmanager usb-modeswitch xmm-modem luci-proto-xmm"
 # MODEM TOOLS
 #PACKAGES+=" atinout modeminfo modemband sms-tool luci-app-modeminfo luci-app-modemband luci-app-sms-tool-js picocom minicom"
 #PACKAGES+=" modeminfo-serial-dell modeminfo-serial-fibocom modeminfo-serial-sierra modeminfo-serial-tw modeminfo-serial-xmm"
-PACKAGES+=" atinout modeminfo sms-tool luci-app-modeminfo picocom minicom"
+PACKAGES+=" atinout sms-tool picocom minicom"
 PACKAGES+=" modeminfo-serial-dell modeminfo-serial-fibocom modeminfo-serial-sierra modeminfo-serial-tw modeminfo-serial-xmm"
 
+
 # VPN TUNNEL
-OPENCLASH="coreutils-nohup ipset ip-full libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag kmod-nft-tproxy luci-app-openclash"
+OPENCLASH="coreutils-nohup ipset ip-full libcap libcap-bin ruby ruby-yaml kmod-tun kmod-inet-diag kmod-nft-tproxy"
 NIKKI="nikki luci-app-nikki"
 INSOMCLASH="insomclash luci-app-insomclash"
-NEKO="php8 php8-cgi kmod-tun bash curl jq ip-full ca-bundle sing-box mihomo luci-app-neko"
+NEKO="php8 php8-cgi kmod-tun bash curl jq ip-full ca-bundle"
 #PASSWALL="microsocks dns2socks dns2tcp ipt2socks tcping chinadns-ng xray-core xray-plugin naiveproxy trojan-plus tuic-client luci-app-passwall"
 
 add_tunnel_packages() {
@@ -61,9 +62,9 @@ add_tunnel_packages() {
         insomclash)
             PACKAGES+=" $INSOMCLASH"
             ;;
-        passwall)
-            PACKAGES+=" $PASSWALL"
-            ;;
+        #passwall)
+            #PACKAGES+=" $PASSWALL"
+            #;;
         nikki-passwall)
             PACKAGES+=" $NIKKI $PASSWALL"
             ;;
@@ -93,21 +94,17 @@ PACKAGES+=" luci-app-3ginfo-lite luci-app-netmonitor"
 
 # THEMES & REMOTE ACCESS
 #PACKAGES+=" luci-theme-argon luci-theme-rtawrt luci-theme-alpha"
-#PACKAGES+=" tailscale luci-app-tailscale"
-PACKAGES+=" luci-theme-argon luci-theme-material"
 PACKAGES+=" tailscale luci-app-tailscale"
+PACKAGES+=" luci-theme-argon luci-theme-material"
 
 # PHP8
 PACKAGES+=" php8 php8-cli php8-fastcgi php8-fpm php8-mod-session php8-mod-ctype php8-mod-fileinfo php8-mod-zip php8-mod-iconv php8-mod-mbstring"
 
 # MISC PACKAGES
 #MISC+=" zoneinfo-core zoneinfo-asia jq htop httping adb openssh-sftp-server zram-swap screen \
-#atc-fib-l8x0_gl atc-fib-fm350_gl luci-proto-atc luci-app-droidnet luci-app-ipinfo \
-#luci-app-lite-watchdog luci-app-poweroffdevice luci-app-ramfree luci-app-tinyfm luci-app-ttyd luci-app-mactodong"
+#atc-fib-l8x0_gl atc-fib-fm350_gl luci-proto-atc luci-app-ramfree luci-app-tinyfm luci-app-ttyd"
 MISC+=" zoneinfo-core zoneinfo-asia jq htop httping adb openssh-sftp-server zram-swap screen \
-atc-fib-l8x0_gl atc-fib-fm350_gl luci-proto-atc luci-app-ipinfo \
-luci-app-poweroffdevice luci-app-tinyfm luci-app-ttyd luci-app-mactodong \
-luci-app-ramfree"
+atc-fib-l8x0_gl atc-fib-fm350_gl luci-proto-atc luci-app-tinyfm luci-app-ttyd"
 
 # PROFILE SPECIFIC
 configure_profile_packages() {
@@ -128,10 +125,10 @@ configure_profile_packages() {
 # RELEASE SPECIFIC
 configure_release_packages() {
     if [[ "${BASE:-}" == "openwrt" ]]; then
-        MISC+=" wpad-openssl iw iwinfo wireless-regdb kmod-cfg80211 kmod-mac80211 luci-app-temp-status"
+        MISC+=" wpad-openssl luci-app-temp-status"
         EXCLUDED+=" -dnsmasq"
     elif [[ "${BASE:-}" == "immortalwrt" ]]; then
-        MISC+=" wpad-openssl iw iwinfo wireless-regdb kmod-cfg80211 kmod-mac80211"
+        MISC+=" wpad-openssl"
         EXCLUDED+=" -dnsmasq -cpusage -automount -libustream-openssl -default-settings-chn -luci-i18n-base-zh-cn"
         
         if [[ "${ARCH_2:-}" == "x86_64" ]] || [[ "${ARCH_2:-}" == "i386" ]]; then
